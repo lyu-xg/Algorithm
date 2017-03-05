71efd763c75622cdbb4aa2ab2c5cc1a061ae8e35
Xueguang Lu <lu.xue@husky.neu.edu>
2017-03-04 23:24:06 -0800

    finishd dynamic programming version

--- CodingHW.py
+++ CodingHW.py
@@ -36,12 +36,13 @@ def readInput():
 class Solution(object):
     def __init__(self,lines):
         
-        self.L2R = {}
-        self.R2L = {}
+        self.L2R = defaultdict()
+        self.R2L = defaultdict()
         for a,b in lines:
             self.L2R[a] = b
             self.R2L[b] = a
         self.N = max(max(self.L2R.keys()),max(self.R2L.keys()))
+        '''
         self.Ldown = [0]*self.N
         self.Rdown = [0]*self.N
         for n in xrange(1,self.N):
@@ -51,47 +52,53 @@ class Solution(object):
         print self.R2L
         print self.Ldown
         print self.Rdown
+        '''
     def intersections(self):
         res = 0
-        for i in xrange(self.N):
-            #print res
+        for i in xrange(self.N+1):
+            #print '\n\nentering iteration {}, res is {}'.format(i,res)
             L = (i,self.L2R[i]) if i in self.L2R else None
             R = (self.R2L[i],i) if i in self.R2L else None
+            #print 'Found {} and {}'.format(L,R)
             if L and self.GoesDownL(i):
-                #print self.RRange(L[1]+1,i-1)
-                res += self.RRange(L[1]+1,i-1) + self.Rdown[i-1]-self.Rdown[L[1]]
+                #print 'Finding leftRange {} to {}'.format(L[1]+1,i-1)
+                rightRange = self.getLefts(L[1]+1,i-1)
+                #print 'Got leftRange:', rightRange
+                res += rightRange
             if R and self.GoesDownR(i):
-                print 'R',R
-                print 'RDOWN',self.Rdown[i-1],self.Rdown[R[1]]
-                print 'RINDEX',i-1,R[1]
-                print self.LRange(R[1]+1,i-1)
-                res += self.LRange(R[1]+1,i-1) + self.Ldown[i-1]-self.Ldown[R[0]]
-            if L and R:
+                #print 'Finding rightRange {} to {}'.format(R[0]+1,i-1)
+                leftRange = self.getRights(R[0]+1,i-1)
+                #print 'Got rightRange', leftRange
+                res += leftRange
+            if L and R and self.GoesDownR(i) and self.GoesDownL(i):
                 res += 1 if isInter(L,R) else 0
-            print res
+            
         return res
 
     def GoesDownL(self,l):
-        return isDown(True,(l,self.L2R[l]))
+        return l > self.L2R[l]
     def GoesDownR(self,r):
-        return isDown(False,(self.R2L[r],r))
+        return self.R2L[r] < r
 
 
     # get corresponding range that !goes-down
     def getRights(self,i,j):
-        return [self.L2R[l] for l in xrange(i,j+1) if l in self.L2R and not self.GoesDownL(l)]
+        # using left i,j to get set of rights
+        count = 0
+        for l in xrange(i,j+1):
+            if l not in self.L2R: continue
+            r = self.L2R[l]
+            if r<=j:
+                count+=1
+        return count
     def getLefts(self,i,j):
-        return [self.R2L[r] for r in xrange(i,j+1) if r in self.R2L and not self.GoesDownR(r)]
-
-    # return how many lines in (i,j) which !goes-down yet still end in the range
-    def LRange(self, i, j):
-        elements = [e for e in self.getRights(i,j) if e>=i and e<=j]
-        print 'LRange',elements
-        return len(elements)
-    def RRange(self, i, j):
-        elements = [e for e in self.getLefts(i,j) if e>=i and e<=j]
-        print 'RRange',elements
-        return len(elements)
+        count = 0
+        for r in xrange(i,j+1):
+            if r not in self.R2L: continue
+            l = self.R2L[r]
+            if l<=j: 
+                count+=1
+        return count
 
     def status(self):
         print 'got {} lines'.format(len(self.L2R))
@@ -113,7 +120,7 @@ def iterativeFindAll(lines):
         outNodes = set(i for i in outNodes if i not in visited)
         visited.add(line)
         result+=sum(isInter(line,node) for node in outNodes)
-    return result\
+    return result
 
 
 def bruteFindAll(lines):
@@ -132,17 +139,19 @@ def testing(n):
     shuffle(right)
     lines = [(left[i],right[i]) for i in range(length)]
     lines = lines[length/2:]
-    print lines
+    #print lines
     bfa = bruteFindAll(lines)
+    print bfa
     S = Solution(lines)
-    S.status()
     nfa = S.intersections()#iterativeFindAll(lines)# S.recursiveFindAll()
-    print bfa,nfa
+    print nfa
     return bfa==nfa
 
 if __name__=="__main__":
-    #length = 20
-    #print all( testing(length) for i in range(100))
-    lines = [(9, 17), (8, 9), (17, 3), (6, 15), (1, 19), (12, 14), (0, 1), (18, 10), (19, 2), (15, 13)]
-    print Solution(lines).intersections()
-    print bruteFindAll(lines)
+    length = 10000
+    print all( testing(length) for i in range(3))
+    #lines = [(1, 4), (0, 9), (3, 2), (7, 3), (8, 0)]
+    #S = Solution(lines)
+    #result = S.intersections()
+    #print 'Final Result:' , result
+    #print bruteFindAll(lines)
